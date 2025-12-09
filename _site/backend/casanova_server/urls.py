9b581f08b1a44665a90a9b66a8e1497cdc7941cb
jiyoonmoon031-design <jiyoonmoon031@gmail.com>
2025-12-10 00:47:53 +0900

    Chore: Remove build artifacts and redundant demo files

--- _site/backend/casanova_server/urls.py
+++
@@ -1,9 +0,0 @@
-# backend/casanova_server/urls.py
-
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/", include("casanova_app.urls")),  # 추가
-]
